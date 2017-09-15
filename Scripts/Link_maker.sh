cd049195f5b248adb43b7f4d1d5b8a2f45d4655d
Erik <eeckee@gmail.com>
2017-09-15 20:42:23 +0200

    update

---
+++ Scripts/Link_maker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Script that makes links 
+
+# Conf files
+ln -sf ~/.files/vimrc ~/.vimrc
+ln -sf ~/.files/zshrc ~/.zshrc
+ln -sf ~/.files/inputrc ~/.inputrc
+ln -sf ~/.files/bashrc ~/.bashrc
+ln -sf ~/.files/zathurarc ~/.config/zathura/zathurarc
+ln -sf ~/.files/tmux.conf ~/.tmux.conf
+ln -sf ~/.files/gitignore ~/.gitignore
+ln -sf ~/.files/gitconfig ~/.gitconfig
+ln -sf ~/.files/xinitrc ~/.xinitrc
+ln -sf ~/.files/Xresources ~/.Xresources
+
+# i3
+ln -sf ~/.files/i3/i3status.conf ~/.config/i3/i3status.conf
+ln -sf ~/.files/i3/config ~/.config/i3/config
+
