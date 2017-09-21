da8b6ce3dcca0f7a379baf7e24ed6179e928d252
Erik <eeckee@gmail.com>
2017-09-21 15:24:32 +0200

    fixed echoes

--- Installation/Installer.sh
+++ Installation/Installer.sh
@@ -17,25 +17,25 @@ fi
 # Upgrading
 echo Upgrading "(apt-get update/upgrade/dist-upgrade)"
 apt-get update
-apt-get upgrade
-apt-get dist-upgrade
+apt-get upgrade -y
+apt-get dist-upgrade -y
 
 # notify-send, working version (otherwise the -t flag did not do anything)
 add-apt-repository ppa:leolik/leolik 
 apt-get update
-apt-get upgrade
-apt-get install libnotify-bin
+apt-get upgrade -y
+apt-get install libnotify-bin -y
 pkill notify-osd
 
 # (some extra functions recommended in solition to the above.
 add-apt-repository ppa:nilarimogard/webupd8
 apt update
-apt-get install notifyosdconfig
+apt-get install notifyosdconfig -y
 
 # Install needed programs
 echo "########################################################"
 echo
-echo Installing programs (git/tmux and the like...)
+echo Installing programs "(git/tmux and the like...)"
 apt-get install git tmux xclip zsh feh curl unclutter vim-gtk udiskie zathura rxvt-unicode-256color ranger wget curl -y
 echo Done!
 echo "########################################################"
@@ -81,12 +81,12 @@ apt-get install i3 -y
 # Dependencies
 # Ubuntu (>= 14.04 LTS, <= 16.04)
 echo Installing i3-gaps dependencies...
-apt-get install bxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm-dev
+apt-get install bxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm-dev -y
 
 # extra dependency
 add-apt-repository ppa:aguignard/ppa
 apt-get update
-apt-get install libxcb-xrm-dev
+apt-get install libxcb-xrm-dev -y
 
 # Install (https://github.com/Airblader/i3/wiki/Compiling-&-Installing)
 echo Download and make i3-gaps in ~/i3-gaps...
@@ -120,6 +120,7 @@ chmod -s /bin/zsh
 
 echo Create links for config files...
 cd ~/.files/Installation
+
 ./Link_maker
 
 #################################################
