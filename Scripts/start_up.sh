137deaf6b14744c1214b11d17f78f8c542833603
Erik <eeckee@gmail.com>
2017-09-22 19:19:53 +0200

    updated start_up

--- Scripts/start_up.sh
+++ Scripts/start_up.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Add this to your .xinitrc or whatever file starts programs on startup.
 # -R restores the last colorscheme that was in use.
-WAL=$(which wal)
-$WAL -R -a 70 
-#DISPLAY=:3 notify-send -t 2000 --urgency=critical --icon=$Img "$wal -R -a 70"
+xrdb ~/.Xdefaults                                                                          
 xmodmap ~/.files/Scripts/speedswapper
 xset r rate 300 75
+WAL=$(which wal)
+$WAL -R -a 70 
 unclutter &
