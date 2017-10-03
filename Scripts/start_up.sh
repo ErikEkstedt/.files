8079bc093ca93abf3f1f9446f1ada813065830c2
Erik <eeckee@gmail.com>
2017-10-03 15:50:12 +0200

    updated startup to change altgr to alt

--- Scripts/start_up.sh
+++ Scripts/start_up.sh
@@ -3,6 +3,8 @@
 # -R restores the last colorscheme that was in use.
 xrdb ~/.Xdefaults                                                                          
 xmodmap ~/.files/Scripts/speedswapper
+xmodmap -e "clear mod5"
+xmodmap -e "keycode 108 = Alt_L"
 xset r rate 300 75
 WAL=$(which wal)
 $WAL -R -a 70 
