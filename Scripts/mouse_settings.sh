1c88ccbe4b372c1c992169a7d83ce390520d84a7
Erik <eeckee@gmail.com>
2017-10-09 14:09:44 +0200

    Added a script that finds a Microsoft mouse and changes some settings

---
+++ Scripts/mouse_settings.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+for id in `xinput --list|grep 'Microsoft'|perl -ne 'while (m/id=(\d+)/g){print "$1\n";}'`; do
+    # echo "setting device ID $id"
+    notify-send -t 500  'Mouse fixed'
+    # xinput set-prop $id "Device Accel Velocity Scaling" 10
+    xinput set-prop $id "Device Accel Constant Deceleration" 2.4
+    xinput set-prop $id "Evdev Scrolling Distance"  -1 -1 1
+done
+
+
+
