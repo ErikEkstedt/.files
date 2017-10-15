b42f085f30da60783aa368ae551fba9f86ecb5a5
Erik <eeckee@gmail.com>
2017-10-15 20:21:41 +0200

    (re)Added wal on startup. defaults are, as of now, ugly.

--- Scripts/start_up.sh
+++ Scripts/start_up.sh
@@ -4,7 +4,7 @@
 xrdb ~/.Xdefaults                                                                          
 xmodmap ~/.files/Scripts/speedswapper
 xset r rate 200 75
-# WAL=$(which wal)
-# $WAL -R -a 70 
+WAL=$(which wal)
+$WAL -R -a 70 
 sh ./mouse_settings.sh &
 unclutter &
