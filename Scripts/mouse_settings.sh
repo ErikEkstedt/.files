09e6c2926ee07c533e555517fb116ff0b7d0b62e
Erik <eeckee@gmail.com>
2017-10-17 12:36:10 +0200

    added steelseries mouse fix

--- Scripts/mouse_settings.sh
+++ Scripts/mouse_settings.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Microsoft mouse
 for id in `xinput --list|grep 'Microsoft'|perl -ne 'while (m/id=(\d+)/g){print "$1\n";}'`; do
     # echo "setting device ID $id"
     notify-send -t 500  'Mouse fixed'
@@ -7,5 +8,10 @@ for id in `xinput --list|grep 'Microsoft'|perl -ne 'while (m/id=(\d+)/g){print "
     xinput set-prop $id "Evdev Scrolling Distance"  1 -1 1
 done
 
-
-
+# SteelSeries mouse
+for id in `xinput --list|grep 'SteelSeries'|perl -ne 'while (m/id=(\d+)/g){print "$1\n";}'`; do
+    # echo "setting device ID $id"
+    # xinput set-prop $id "Device Accel Velocity Scaling" 10
+    xinput set-prop $id "Device Accel Constant Deceleration" 2.4
+    xinput set-prop $id "Evdev Scrolling Distance"  1 -1 1
+done
