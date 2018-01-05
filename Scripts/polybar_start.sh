e45e9844396463f814fd86f33d95f578171b3439
Erik <eeckee@gmail.com>
2018-01-05 22:51:00 +0100

    this is nice

--- Scripts/polybar_start.sh
+++ Scripts/polybar_start.sh
@@ -1,7 +1,16 @@
-#!/usr/bin/env sh
+#!/usr/bin/zsh
 
 killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-polybar laptopbar -c ~/.files/polybar.conf &
+killall notify-osd
+
+if [ "$HOST" = "erik-Desktop" ]
+then
+	DISPLAY=:0 notify-send -t 3000 --urgency=critical "DESKTOP POLYBAR"
+	polybar desktopbar -c ~/.files/polybar.conf &
+else
+	DISPLAY=:0 notify-send -t 3000 --urgency=critical "Laptop POLYBAR"
+	polybar laptopbar -c ~/.files/polybar.conf &
+fi
