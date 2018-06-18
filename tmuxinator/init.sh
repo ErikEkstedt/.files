87b8fce3a19db4471b8da159d260249f55f21da9
Erik <eeckee@gmail.com>
2018-06-18 10:19:34 +0200

    up

---
+++ tmuxinator/init.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+# https://github.com/tmuxinator/tmuxinator 
+
+# Going to require sudo. How to install gem as user ?
+gem install tmuxinator -y
+
+# Link config files and add completion to zshrc
+# TODO
+# mkdir -p ~/.config/tmuxinator
+# ln -sf ~/.files/tmuxinator/dropdown ~/.config/tmuxinator
+# ln -sf ~/.files/tmuxinator/project0 ~/.config/tmuxinator
+
+
+
+
