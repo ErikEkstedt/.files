c92cf131140e09b961d569c32b9e963b62885775
Erik <eeckee@gmail.com>
2018-07-01 16:00:18 +0200

    added

--- nvim/install.sh
+++ nvim/install.sh
@@ -6,7 +6,6 @@ pip install neovim
 echo Installing Nevim NPM package
 npm install -g neovim
 
-
 echo "Add nvim PPA, update system and install"
 sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update -y
@@ -14,3 +13,4 @@ sudo apt-get install neovim -y
 
 echo ln -sf ~/.files/nvim ~/.config/
 ln -sf ~/.files/nvim ~/.config/
+ln -sf ~/.files/nvim/init.vim ~/.vimrc
