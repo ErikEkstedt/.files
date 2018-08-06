8310ef6fe6491815d0a9e1daa8927981e7bccc30
Erik <eeckee@gmail.com>
2018-08-06 17:18:24 +0200

    Upgraded install file for linux/MacOS equivalence

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-echo Installing Nevim Python package
-pip install neovim
+case `uname` in
+    Darwin)
+		brew install neovim
+            ;;
+    Linux)
+		echo "Add nvim PPA, update system and install"
+		sudo add-apt-repository ppa:neovim-ppa/stable
+		sudo apt-get update -y
+		sudo apt-get install neovim -y
 
-echo Installing Nevim NPM package
-npm install -g neovim
+		echo Installing Nevim pip package
+		pip install neovim
 
-echo "Add nvim PPA, update system and install"
-sudo add-apt-repository ppa:neovim-ppa/stable
-sudo apt-get update -y
-sudo apt-get install neovim -y
+		echo Installing Nevim NPM package
+		npm install -g neovim
+        ;;
+esac
 
 echo ln -sf ~/.files/nvim ~/.config/
 ln -sf ~/.files/nvim ~/.config/
