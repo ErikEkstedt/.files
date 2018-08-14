7cd984943edaa0ba192af527be5ff952510fa214
Erik <eeckee@gmail.com>
2018-08-14 17:09:46 +0200

    Rename: README -> Install

---
+++ Collection/README.md
@@ -0,0 +1,9 @@
+# Data Collection
+
+I want to record data.
+I want to record keystrokes.
+I want to capture my screen.
+I want to capture weveral webcams.
+I want to capture Audio.
+
+ScreenCapture: [OBS](https://github.com/NVIDIA/tacotron2.git)
--- Collection/install.sh
+++ Collection/install.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 
-# Install kazam
-
-sudo apt install kazam
-sudo apt remove kazam
-
-# Install OBS-studio
-sudo add-apt-repository ppa:obsproject/obs-studio
-sudo apt update
-sudo apt install obs-studio
-
-# Install simplescreenrecorder
-sudo apt install simplescreenrecorder
-
+echo "Install Obs-studio? (y/n)"
+read answer
+if [[ $answer == 'y' || $answer == 'Y'   ]]; then
+	# Install OBS-studio
+	case Linux in
+		Darwin)
+			# commands for OS X go here
+			echo "Go to this website"
+			echo "https://github.com/obsproject/obs-studio/releases"
+			;;
+		Linux)
+			# commands for Linux go here
+			sudo add-apt-repository ppa:obsproject/obs-studio
+			sudo apt update
+			sudo apt install obs-studio
+			;;
+	esac
+fi
