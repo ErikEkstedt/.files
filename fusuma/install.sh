463d887c82726b2a0cb7df420df123d00499c62e
Erik <eeckee@gmail.com>
2018-08-14 13:47:21 +0200

    Fixed fusuma install script (only linux)

--- fusuma/install.sh
+++ fusuma/install.sh
@@ -2,13 +2,43 @@
 
 prg=fusuma
 if ! [ -x "$(command -v $prg)" ]; then
-	echo "Installing $prg" >&2
+	echo "1. Grant permission to read the touchpad device"
+	echo "IMPORTANT: You MUST be a member of the INPUT group to read touchpad by Fusuma."
+	echo "-> sudo gpasswd -a $USER input? (y/n)"
+	read answer
+	if [[ $answer == 'y' || $answer == 'Y'   ]]; then
+		sudo gpasswd -a $USER input
+		echo "Then, You MUST LOGOUT/LOGIN or REBOOT to assign this group."
+
+		echo "Restart now? (y/n)"
+		read answer
+		if [[ $answer == 'y' || $answer == 'Y'   ]]; then
+			sudo reboot
+		fi
+	fi
+
+	echo "2. Install libinput-tools. You need libinput release 1.0 or later."
+	echo "Install libinput-tools? (y/n)"
+	read answer
+	if [[ $answer == 'y' || $answer == 'Y'   ]]; then
+		sudo apt-get install libinput-tools
+	fi
+
+	echo "3. Install Fusuma? (y/n)"
+	read answer
+	if [[ $answer == 'y' || $answer == 'Y'   ]]; then
+		sudo gem install fusuma
+	fi
+
 else
 	echo "$prg is already installed!" >&2
 fi
 
-echo 
-echo "Linking config files:"
-echo "$HOME/.files/fusuma/config.yml -> $HOME/.config/fusuma/config.yml"
-
-ln -sf $HOME/.files/fusuma/config.yml $HOME/.config/fusuma/config.yml
+echo "Link configs? (y/n)"
+read answer
+if [[ $answer == 'y' || $answer == 'Y'   ]]; then
+	echo "Linking config files:"
+	echo "$HOME/.files/fusuma/config.yml -> $HOME/.config/fusuma/config.yml"
+	mkdir -p $HOME/.config/fusuma
+	ln -sf $HOME/.files/fusuma/config.yml $HOME/.config/fusuma/config.yml
+fi
