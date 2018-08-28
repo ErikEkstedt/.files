218b75a762512a09910985d47ea7dfcd6ec81b6a
Erik <eeckee@gmail.com>
2018-08-28 12:14:28 +0200

    Added directory for jupyter settings

---
+++ jupyter/custom/custom.js
@@ -0,0 +1,11 @@
+
+alert("Run all cells")
+
+Jupyter.keyboard_manager.command_shortcuts.add_shortcut('r', {
+    help : 'run all cells',
+    help_index : 'zz',
+    handler : function (event) {
+        IPython.notebook.execute_all_cells();
+        return false;
+    }}
+);
