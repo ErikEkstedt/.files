f778d1f6d2cd1d47a95de964adfdc241579c57d2
Erik <eeckee@gmail.com>
2018-10-07 20:58:30 +0200

    tmux: new tmux abilities. send command to all panes. Prefix+= even layout

---
+++ tmux/tmux_functions.zsh
@@ -0,0 +1,14 @@
+# When specifying keys, most represent themselves (for example ‘A’ to ‘Z’). 
+# Ctrl keys may be prefixed with ‘C-’ or ‘^’, and Alt (meta) with ‘M-’.
+# The following special key names are accepted:
+# Up, Down, Left, Right, BSpace, BTab, DC (Delete), End, Enter, Escape, F1 to F20,
+# Home, IC (Insert), NPage/PageDown/PgDn, PPage/PageUp/PgUp, Space, and Tab.
+
+# Send keys command to all panes
+# Example: _tmux_send_keys_all_panes_ ls Enter
+_tmux_send_keys_all_panes_ () {
+  for _pane in $(tmux list-panes -F '#P'); do
+    tmux send-keys -t ${_pane} "$@" Enter
+  done
+}
+
