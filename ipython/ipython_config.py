a7483b5deef3b291bfda00c0a00ee8446ff74858
Erik <eeckee@gmail.com>
2018-10-19 15:31:20 +0200

    ipython: color and highlight in config

--- ipython/ipython_config.py
+++ ipython/ipython_config.py
@@ -181,10 +181,10 @@ c.InteractiveShell.autoindent = True
 ## Use colors for displaying information about objects. Because this information
 #  is passed through a pager (like 'less'), and some pagers get confused with
 #  color codes, this capability can be turned off.
-#c.InteractiveShell.color_info = True
+c.InteractiveShell.color_info = True
 
 ## Set the color scheme (NoColor, Neutral, Linux, or LightBG).
-#c.InteractiveShell.colors = 'Neutral'
+# c.InteractiveShell.colors = 'Linux'
 
 ##
 #c.InteractiveShell.debug = False
@@ -304,7 +304,7 @@ c.TerminalInteractiveShell.highlight_matching_brackets = True
 
 ## The name or class of a Pygments style to use for syntax highlighting. To see
 #  available styles, run `pygmentize -L styles`.
-#c.TerminalInteractiveShell.highlighting_style = traitlets.Undefined
+c.TerminalInteractiveShell.highlighting_style = 'monokai'
 
 ## Override highlighting format for specific tokens
 #c.TerminalInteractiveShell.highlighting_style_overrides = {}
