ef56546366373497f9bb980051b3af0bc75c5271
Erik <eeckee@gmail.com>
2019-01-11 00:21:03 +0100

    touchpad and install for linux on macbookpro

--- Installation/MBP/linux/link-hwdb.sh
+++ Installation/MBP/linux/link-hwdb.sh
@@ -2,6 +2,5 @@
 
 echo "Link hwdb files to /etc/udev/hwdb"
 
-sudo cp $HOME/.files/Installation/MBP-linux/61-evdev-local.hwdb /etc/udev/hwdb.d/61-evdev-local.hwdb 
-
-sudo cp $HOME/.files/Installation/MBP-linux/61-libinput-local.hwdb /etc/udev/hwdb.d/61-libinput-local.hwdb 
+sudo cp $HOME/.files/Installation/MBP/linux/61-evdev-local.hwdb /etc/udev/hwdb.d/61-evdev-local.hwdb 
+sudo cp $HOME/.files/Installation/MBP/linux/61-libinput-local.hwdb /etc/udev/hwdb.d/61-libinput-local.hwdb 
