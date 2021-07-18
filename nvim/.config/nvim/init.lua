5501bf4d02aed83402177b1e0432fecb4b0c81b3
Erik <eeckee@gmail.com>
2021-07-18 16:36:30 +0200

    nvim: init got formatted

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,29 +1,29 @@
 -- Init.lua
 
 -- set mapleader early prior to plugins
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ','
+vim.g.mapleader = " "
+vim.g.maplocalleader = ","
 
 -- Load plugins (specific plugin settings lua/plugins/<plugName>.lua)
-require('plugins')
-require('plugins.nvim-bufferline') -- Error in packercompile
+require("plugins")
+require("plugins.nvim-bufferline") -- Error in packercompile
 
 -- options/settings
-require('settings')
+require("settings")
 
 -- Python
-vim.g.HOME = vim.fn.expand('$HOME')
+vim.g.HOME = vim.fn.expand("$HOME")
 vim.g.loaded_python_provider = 0
-vim.g.python3_host_prog=vim.g.HOME .. '/miniconda3/envs/neovim3/bin/python'
-vim.g.python_host_bin=vim.g.HOME .. '/miniconda3/envs/neovim3/bin'
-vim.g.BROWSER	= 'brave-browser'
+vim.g.python3_host_prog = vim.g.HOME .. "/miniconda3/envs/neovim3/bin/python"
+vim.g.python_host_bin = vim.g.HOME .. "/miniconda3/envs/neovim3/bin"
+vim.g.BROWSER = "brave-browser"
 
 -- Mappings Normal etc
-require('mappings')
+require("mappings")
 -- require('plugins.neuronCustom')
 
 -- Set Colorscheme
-vim.cmd('colorscheme onebuddy')
+vim.cmd("colorscheme onebuddy")
 -- Colorscheme clears hi for hop?
 vim.cmd("hi HopNextKey gui=bold guifg=cyan")
 vim.cmd("hi HopNextKey1 gui=bold guifg=cyan")
