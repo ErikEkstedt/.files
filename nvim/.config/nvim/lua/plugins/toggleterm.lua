579bd72e2715a506d86b0ed879faac9a011d1eae
Erik <eeckee@gmail.com>
2021-07-18 16:07:47 +0200

    nvim: added lf toggleterm function

--- nvim/.config/nvim/lua/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -17,4 +17,20 @@ function _lazygit_toggle()
   lazygit:toggle()
 end
 
+local lf = Terminal:new({
+  cmd = "lf",
+  hidden = true,
+  direction = "float",
+  float_opts = {
+    border = "single",
+    height = math.floor(vim.o.lines * .8),
+    width = math.floor(vim.o.columns * .8),
+  },
+})
+
+function _lf_toggle()
+  lf:toggle()
+end
+
 vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>u", "<cmd>lua _lf_toggle()<CR>", {noremap = true, silent = true})
