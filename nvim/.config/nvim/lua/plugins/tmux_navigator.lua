4d31f0f3ae00852ea34f4847bc376177a75f768e
Erik <eeckee@gmail.com>
2021-07-25 18:00:59 +0200

    nvim: tmux remove message

--- nvim/.config/nvim/lua/plugins/tmux_navigator.lua
+++ nvim/.config/nvim/lua/plugins/tmux_navigator.lua
@@ -7,4 +7,3 @@ vim.api.nvim_set_keymap("n", "<M-h>", ":TmuxNavigateLeft<CR>", {noremap = true, 
 vim.api.nvim_set_keymap("n", "<M-j>", ":TmuxNavigateDown<CR>", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<M-k>", ":TmuxNavigateUp<CR>", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<M-l>", ":TmuxNavigateRight<CR>", {noremap = true, silent = true})
-print("TmuxNavigator")
