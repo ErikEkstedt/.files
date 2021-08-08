8b7f0b6dde571f2d2ca7c495915a999e92a58374
Erik <eeckee@gmail.com>
2021-08-08 20:49:50 +0200

    nvim: fixed pyright sluggishness

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,32 +1,38 @@
 -- local conf = require('lspconfig')
-local nvim_lsp = require('lspconfig')
 
 -- Signs
-vim.fn.sign_define("LspDiagnosticsSignError", {text="!", texthl="LspDiagnosticsSignError", numhl="LspDiagnosticsSignError"})
-vim.fn.sign_define("LspDiagnosticsSignWarning", {text="?", texthl="LspDiagnosticsSignWarning", numhl="LspDiagnosticsSignWarning"})
-vim.fn.sign_define("LspDiagnosticsSignInformation", {text="I", texthl="LspDiagnosticsSignInformation"})
-vim.fn.sign_define("LspDiagnosticsSignHint", {text="", texthl="LspDiagnosticsSignHint"})
-
+vim.fn.sign_define(
+  "LspDiagnosticsSignError",
+  {text = "!", texthl = "LspDiagnosticsSignError", numhl = "LspDiagnosticsSignError"}
+)
+vim.fn.sign_define(
+  "LspDiagnosticsSignWarning",
+  {text = "?", texthl = "LspDiagnosticsSignWarning", numhl = "LspDiagnosticsSignWarning"}
+)
+vim.fn.sign_define("LspDiagnosticsSignInformation", {text = "I", texthl = "LspDiagnosticsSignInformation"})
+vim.fn.sign_define("LspDiagnosticsSignHint", {text = "", texthl = "LspDiagnosticsSignHint"})
 
 local on_attach = function(client, bufnr)
-	local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_keymap(...)
+    vim.api.nvim_buf_set_keymap(bufnr, ...)
+  end
 
   -- shows which field you are currently on when writing function args
   require "lsp_signature".on_attach()
 
-	-- Mappings.
-	local opts = { noremap=true, silent=true }
-	buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-	buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-	buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-	buf_set_keymap('n', '<leader>rr', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  -- Mappings.
+  local opts = {noremap = true, silent = true}
+  buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+  buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+  buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+  buf_set_keymap("n", "<leader>rr", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
 
-	-- Set some keybinds conditional on server capabilities
-	if client.resolved_capabilities.document_formatting then
-		buf_set_keymap("n", "<space>fo", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-	elseif client.resolved_capabilities.document_range_formatting then
-		buf_set_keymap("n", "<space>fr", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
-	end
+  -- Set some keybinds conditional on server capabilities
+  if client.resolved_capabilities.document_formatting then
+    buf_set_keymap("n", "<space>fo", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+  elseif client.resolved_capabilities.document_range_formatting then
+    buf_set_keymap("n", "<space>fr", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+  end
 end
 
 -- Configure lua language server for neovim development
@@ -34,20 +40,20 @@ local lua_settings = {
   Lua = {
     runtime = {
       -- LuaJIT in the case of Neovim
-      version = 'LuaJIT',
-      path = vim.split(package.path, ';'),
+      version = "LuaJIT",
+      path = vim.split(package.path, ";")
     },
     diagnostics = {
       -- Get the language server to recognize the `vim` global
-      globals = {'vim'},
+      globals = {"vim"}
     },
     workspace = {
       -- Make the server aware of Neovim runtime files
       library = {
-        [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-        [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
-      },
-    },
+        [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+        [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true
+      }
+    }
   }
 }
 
@@ -59,19 +65,17 @@ local function make_config()
     -- enable snippet support
     capabilities = capabilities,
     -- map buffer local keybindings when the language server attaches
-    on_attach = on_attach,
+    on_attach = on_attach
   }
 end
 
 -- lsp-install
 local function setup_servers()
-  require'lspinstall'.setup()
+  require "lspinstall".setup()
 
   -- get all installed servers
-  local servers = require'lspinstall'.installed_servers()
+  local servers = require "lspinstall".installed_servers()
   -- ... and add manually installed servers
-  table.insert(servers, "clangd")
-  table.insert(servers, "sourcekit")
 
   for _, server in pairs(servers) do
     local config = make_config()
@@ -79,22 +83,37 @@ local function setup_servers()
     -- language specific config
     if server == "lua" then
       config.settings = lua_settings
-    end
-    if server == "sourcekit" then
-      config.filetypes = {"swift", "objective-c", "objective-cpp"}; -- we don't want c and cpp!
-    end
-    if server == "clangd" then
-      config.filetypes = {"c", "cpp"}; -- we don't want objective-c and objective-cpp!
+    elseif server == "python" then
+      --[[
+      **pyright was superslow and showed diagnostics on incorrect lines**
+      **the settings below fixed**
+
+      useLibraryCodeForTypes [boolean]:
+      Determines whether pyright reads, parses and analyzes library
+      code to extract type information in the absence of type stub files.
+      Type information will typically be incomplete.
+      !! WE RECOMMEND USING TYPE STUBS WHERE POSSIBLE.
+      THE DEFAULT VALUE FOR THIS OPTION IS FALSE. !!
+      --]]
+      config = {
+        settings = {
+          python = {
+            analysis = {
+              useLibraryCodeForTypes = false
+            }
+          }
+        }
+      }
     end
 
-    require'lspconfig'[server].setup(config)
+    require "lspconfig"[server].setup(config)
   end
 end
 
 setup_servers()
 
 -- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
-require'lspinstall'.post_install_hook = function ()
+require "lspinstall".post_install_hook = function()
   setup_servers() -- reload installed servers
   vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
 end
