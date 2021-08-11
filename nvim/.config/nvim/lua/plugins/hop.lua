da7a0d1bc63a624bf5c8130762b41451925163f4
Erik <eeckee@gmail.com>
2021-08-11 22:42:58 +0200

    nvim: Hop to follow direction instead of listing all.

--- nvim/.config/nvim/lua/plugins/hop.lua
+++ nvim/.config/nvim/lua/plugins/hop.lua
@@ -1,6 +1,6 @@
 -- Hop.nvim
 -- get characters over all words and jump their on press.
-vim.api.nvim_set_keymap('n', '<Leader>j', ':HopLine<CR>', {noremap=true, silent=true})
-vim.api.nvim_set_keymap('n', '<Leader>k', ':HopLine<CR>', {noremap=true, silent=true})
-vim.api.nvim_set_keymap('n', '<Leader>h', ':HopWord<CR>', {noremap=true, silent=true})
-vim.api.nvim_set_keymap('n', '<Leader>l', ':HopWord<CR>', {noremap=true, silent=true})
+vim.api.nvim_set_keymap("n", "<Leader>j", ":HopLineStartAC<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<Leader>k", ":HopLineStartBC<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<Leader>h", ":HopWordBC<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<Leader>l", ":HopWordAC<CR>", {noremap = true, silent = true})
