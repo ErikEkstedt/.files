0f25e564a94fb2bf9500797863eb824b19fdd495
Erik <eeckee@gmail.com>
2021-08-23 23:52:49 +0200

    nvim: commentstring

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,11 +1,44 @@
-local treesitter = require'nvim-treesitter.configs'
+local treesitter = require "nvim-treesitter.configs"
 
 treesitter.setup {
   ensure_installed = {
-    'c', 'cpp', 'dart', 'go', 'html', 'java', 'javascript', 'python', 'ruby',
-    'rust', 'typescript', 'lua'
+    "c",
+    "cpp",
+    "dart",
+    "go",
+    "html",
+    "java",
+    "javascript",
+    "python",
+    "ruby",
+    "rust",
+    "typescript",
+    "lua"
   },
   highlight = {
     enable = true
+  },
+  -- incremental_selection = {
+  --   enable = true,
+  --   keymaps = {
+  --     init_selection = "<C-g><C-g>",
+  --     node_incremental = "<C-g><C-j>",
+  --     scope_incremental = "<C-g><C-s>",
+  --     node_decremental = "<C-g><C-k>"
+  --   }
+  -- },
+  context_commentstring = {
+    enable = true,
+    enable_autocmd = false,
+    config = {
+      javascript = {
+        __default = "// %s",
+        jsx_element = "{/* %s */}",
+        jsx_fragment = "{/* %s */}",
+        jsx_attribute = "// %s",
+        comment = "// %s"
+      }
+    }
   }
+  -- indent = {enable = true}
 }
