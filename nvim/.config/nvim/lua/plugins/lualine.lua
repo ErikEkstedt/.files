cd5af75135ffeaaaf4db7487cc384196dff17589
Erik <eeckee@gmail.com>
2021-11-30 01:20:13 +0100

    nvim: lualine fixin

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -5,8 +5,10 @@ local colors = {
   magenta = "#E80FE7",
   red = "#e06c75",
   orange = "#DCB917",
-  bg = "#404145",
-  bg2 = "#2C2F33",
+  bg = "#3E3F42",
+  -- bg = "2A2C30", -- "#2D3235",
+  bg2 = "#2A2C30", -- "#2D3235",
+  bg_inactive = "#27292B",
   bg_one_dark = "#1F2123",
   color2 = "#0f1419",
   color3 = "#ffee99",
@@ -30,16 +32,18 @@ local theme = {
     z = {fg = colors.red, bg = colors.bg, gui = "bold"}
   },
   inactive = {
-    a = {fg = colors.color4, bg = colors.bg2},
-    b = {fg = colors.color4, bg = colors.bg2},
-    c = {fg = colors.color4, bg = colors.bg2},
-    z = {fg = colors.color4, bg = colors.bg2}
+    a = {fg = colors.color4, bg = colors.bg_inactive},
+    b = {fg = colors.color4, bg = colors.bg_inactive},
+    c = {fg = colors.color4, bg = colors.bg_inactive},
+    x = {fg = colors.color4, bg = colors.bg_inactive},
+    y = {fg = colors.color4, bg = colors.bg_inactive},
+    z = {fg = colors.color4, bg = colors.bg_inactive}
   },
   normal = {
     a = {fg = colors.black, bg = colors.blue, gui = "bold"},
-    b = {fg = colors.magenta, bg = colors.bg},
-    c = {fg = colors.blue, bg = colors.bg2},
-    x = {fg = colors.blue, bg = colors.bg, gui = "bold"},
+    b = {fg = colors.magenta, bg = colors.bg, gui = "bold"},
+    c = {fg = colors.blue, bg = colors.bg2, gui = "bold"},
+    x = {fg = colors.green, bg = colors.bg2, gui = "bold"},
     y = {fg = colors.blue, bg = colors.bg, gui = "bold"},
     z = {fg = colors.blue, bg = colors.bg, gui = "bold"}
   },
@@ -51,116 +55,200 @@ local theme = {
   }
 }
 
+-- LSP
+local get_lsp_client = function(msg)
+  msg = msg or "LSP Inactive"
+  local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+  local clients = vim.lsp.get_active_clients()
+  if next(clients) == nil then
+    return msg
+  end
+  local lsps = ""
+  for _, client in ipairs(clients) do
+    local filetypes = client.config.filetypes
+    if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+      -- print(client.name)
+      if lsps == "" then
+        -- print("first", lsps)
+        lsps = client.name
+      else
+        -- print("more", lsps)
+        if not string.find(lsps, client.name) then
+          lsps = lsps .. ", " .. client.name
+        end
+      end
+    end
+  end
+  if lsps == "" then
+    return msg
+  else
+    return lsps
+  end
+end
+
+-- chris@machine: https://github.com/ChristianChiarulli/LunarVim/blob/rolling/lua/core/galaxyline.lua
+local function env_cleanup(venv)
+  if string.find(venv, "/") then
+    local final_venv = venv
+    for w in venv:gmatch "([^/]+)" do
+      final_venv = w
+    end
+    venv = final_venv
+  end
+  return venv
+end
+
+-- PythonEnv
+local PythonEnv = function()
+  if vim.bo.filetype == "python" then
+    local venv = os.getenv "CONDA_DEFAULT_ENV"
+    if venv ~= nil then
+      return "   (" .. env_cleanup(venv) .. ")"
+    end
+    venv = os.getenv "VIRTUAL_ENV"
+    if venv ~= nil then
+      return "   (" .. env_cleanup(venv) .. ")"
+    end
+    return ""
+  end
+  return ""
+end
+
+---------------------------------------------------
+-- Sections
+local inactive_different = {
+  lualine_a = {
+    {
+      function()
+        return " "
+      end,
+      separator = {left = ""},
+      padding = {right = 0, left = 0}
+    }
+  },
+  lualine_b = {},
+  lualine_c = {
+    {
+      "filename",
+      path = 2,
+      fmt = function(str)
+        local ret = "↑ " .. str .. " ↑"
+        return ret
+      end,
+      shortening_target = 100
+    }
+  },
+  lualine_x = {
+    {
+      "diagnostics",
+      separator = {left = ""},
+      left_padding = 2,
+      sources = {"nvim_lsp"}
+    }
+  },
+  lualine_y = {
+    {
+      "diff",
+      symbols = {
+        added = "  ",
+        modified = " 柳",
+        removed = "  "
+      },
+      padding = 0,
+      separator = {right = ""}
+    },
+    {
+      "branch",
+      icon = "",
+      padding = {left = 1, right = 0},
+      separator = ""
+    }
+  },
+  lualine_z = {
+    {
+      function()
+        return " "
+      end,
+      separator = {right = ""},
+      padding = {right = 0, left = 0}
+    }
+  }
+}
+
+local sections = {
+  lualine_a = {
+    {
+      "mode",
+      -- fmt = function(str)
+      --   return str:sub(1, 1)
+      -- end,
+      separator = {left = "", right = ""},
+      padding = {left = 0, right = 0}
+    }
+  },
+  lualine_b = {
+    {
+      "branch",
+      icon = "",
+      padding = {left = 1, right = 1},
+      separator = {right = ""}
+    },
+    {
+      "diff",
+      symbols = {
+        added = "  ",
+        modified = " 柳",
+        removed = "  "
+      },
+      padding = 0,
+      separator = {right = ""}
+    }
+  },
+  lualine_c = {
+    {
+      "filename",
+      file_status = true, -- displays file status (readonly status, modified status)
+      path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+      shorting_target = 40 -- Shortens path to leave 40 space in the window
+      -- for other components. Terrible name any suggestions?
+    }
+  },
+  lualine_x = {
+    {
+      PythonEnv,
+      separator = {left = ""}
+    },
+    {
+      get_lsp_client,
+      separator = {left = ""},
+      icon = " "
+    },
+    {
+      "diagnostics",
+      sources = {"nvim_lsp"}
+    }
+  },
+  lualine_y = {
+    {"filetype", separator = {left = ""}, padding = {left = 0, right = 1}}
+  },
+  lualine_z = {
+    {"location", separator = {right = ""}, padding = 0},
+    {"progress", separator = {right = ""}, padding = 0}
+  }
+}
+
 require "lualine".setup {
   options = {
     icons_enabled = true,
     theme = theme,
-    -- component_separators = {left = "", right = ""},
-    -- section_separators = {left = "", right = ""},
-    -- component_separators = {left = "|", right = "--"},
-    -- section_separators = {left = "█", right = "--"},
     component_separators = "|",
     section_separators = {left = "", right = ""},
     disabled_filetypes = {},
     always_divide_middle = true
   },
-  sections = {
-    lualine_a = {
-      {
-        "mode",
-        -- fmt = function(str)
-        --   return str:sub(1, 1)
-        -- end,
-        separator = {left = "", right = ""},
-        padding = {left = 0, right = 0}
-      }
-    },
-    lualine_b = {
-      {
-        "branch",
-        icon = "",
-        padding = {left = 1, right = 1},
-        separator = {right = ""}
-      },
-      {
-        "diff",
-        symbols = {
-          added = "  ",
-          modified = " 柳",
-          removed = "  "
-        },
-        padding = 0,
-        separator = {right = ""}
-      }
-    },
-    lualine_c = {
-      {
-        "filename",
-        file_status = true, -- displays file status (readonly status, modified status)
-        path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
-        shorting_target = 40 -- Shortens path to leave 40 space in the window
-        -- for other components. Terrible name any suggestions?
-      }
-    },
-    lualine_x = {
-      {
-        "diagnostics",
-        separator = {left = ""},
-        left_padding = 2,
-        sources = {"nvim_lsp"}
-      }
-    },
-    lualine_y = {
-      {"filetype", separator = {left = ""}, padding = {left = 0, right = 1}}
-    },
-    lualine_z = {
-      {"location", separator = {right = ""}, padding = 0},
-      {"progress", separator = {right = ""}, padding = 0}
-    }
-  },
-  inactive_sections = {
-    lualine_a = {
-      {
-        function()
-          return " "
-        end,
-        separator = {left = ""},
-        padding = {right = 0, left = 0}
-      }
-    },
-    lualine_b = {
-      {
-        "filename",
-        path = 2,
-        fmt = function(str)
-          -- local width = vim.fn.winwidth(0)
-          -- local fill = width - #str
-          -- local horizontal_sep = "—"
-          -- fill = vim.fn.float2nr(fill / 2) - 10
-          -- print("fill: " .. fill .. " w: " .. width)
-          -- local pad = string.rep(horizontal_sep, fill)
-          -- local ret = "↑" .. pad .. str .. pad .. "↑"
-          -- local ret = pad .. "↑ " .. str .. " ↑" .. pad
-          local ret = "↑ " .. str .. " ↑"
-          return ret
-          -- return ""
-        end,
-        shortening_target = 40
-      }
-    },
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = {
-      {
-        function()
-          return " "
-        end,
-        separator = {right = ""},
-        padding = {right = 0, left = 0}
-      }
-    }
-  },
+  sections = sections,
+  inactive_sections = inactive_different,
+  -- inactive_sections = sections,
   tabline = {},
   extensions = {}
 }
