77516370b9ee7b5bd2811d239acc75e103d88cf1
Erik <eeckee@gmail.com>
2021-12-02 21:27:45 +0100

    nvim: deleted hop. using lightspeed

--- nvim/.config/nvim/lua/plugins/hop.lua
+++
@@ -1,8 +0,0 @@
--- Hop.nvim
-require "hop".setup()
-
--- get characters over all words and jump their on press.
-vim.api.nvim_set_keymap("n", "<Leader>j", ":HopLineStartAC<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<Leader>k", ":HopLineStartBC<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<Leader>h", ":HopWordCurrentLineBC<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<Leader>l", ":HopWordCurrentLineAC<CR>", {noremap = true, silent = true})
