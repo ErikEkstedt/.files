25e6e609d3751f1f34a2dff8e9ac3e2c813e7842
Erik <eeckee@gmail.com>
2021-12-05 03:14:07 +0100

    nvim: markdown preview browser on uname

--- nvim/.config/nvim/lua/plugins/markdown-preview.lua
+++ nvim/.config/nvim/lua/plugins/markdown-preview.lua
@@ -1,5 +1,11 @@
 -- MarkdownPreviewToggle
 
 vim.g.mkdp_auto_close = 0
-vim.api.nvim_set_keymap('n', '<Leader><S-c>', '<Plug>MarkdownPreviewToggle', {silent=false})
+
+if vim.loop.os_uname()["sysname"] == "Linux" then
+  vim.g.mkdp_browser = "brave-browser"
+else
+  vim.g.mkdp_browser = "brave"
+end
+vim.api.nvim_set_keymap("n", "<Leader><S-c>", "<Plug>MarkdownPreviewToggle", {silent = false})
 -- vim.api.nvim_set_keymap('n', '<Leader>c', '<Plug>MarkdownPreviewToggle')
