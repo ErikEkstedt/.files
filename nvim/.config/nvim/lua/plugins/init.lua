eb5032e9a649f071726f79dafa67a1b5962b85dd
Erik <eeckee@gmail.com>
2023-01-18 20:50:41 +0100

    nvim: copilot

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -248,6 +248,36 @@ return require("packer").startup(
       use "rebelot/kanagawa.nvim"
 
       if (not vim.g.minimal_plugin) then
+        use {
+          "zbirenbaum/copilot.lua",
+          -- "zbirenbaum/copilot-cmp" == TODO: fix cmp source for copilot
+          cmd = "Copilot",
+          event = "InsertEnter",
+          config = function()
+            vim.schedule(
+              function()
+                require("copilot").setup(
+                  {
+                    suggestion = {
+                      enabled = true,
+                      auto_trigger = true,
+                      debounce = 75,
+                      keymap = {
+                        accept = "<C-l>",
+                        accept_word = false,
+                        accept_line = false,
+                        next = "<M-]>",
+                        prev = "<M-[>",
+                        dismiss = "<C-]>"
+                      }
+                    }
+                  }
+                )
+              end
+            )
+          end
+        }
+
         -- Formatter
         use {
           "mhartington/formatter.nvim",
