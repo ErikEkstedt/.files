37e61c28c1d92d09d5a5bdc62715a215a1f17a92
Erik <eeckee@gmail.com>
2023-02-11 14:37:14 +0100

    nvim: gn/gp to go between new/different Git changes in code

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -8,5 +8,39 @@ require("gitsigns").setup {
   },
   numhl = false,
   linehl = false,
-  keymaps = {}
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+    vim.keymap.set(
+      "n",
+      "<Space>gn",
+      function()
+        if vim.wo.diff then
+          return "]c"
+        end
+        vim.schedule(
+          function()
+            gs.next_hunk()
+          end
+        )
+        return "<Ignore>"
+      end,
+      {expr = true, buffer = bufnr}
+    )
+    vim.keymap.set(
+      "n",
+      "<Space>gp",
+      function()
+        if vim.wo.diff then
+          return "[c"
+        end
+        vim.schedule(
+          function()
+            gs.prev_hunk()
+          end
+        )
+        return "<Ignore>"
+      end,
+      {expr = true, buffer = bufnr}
+    )
+  end
 }
