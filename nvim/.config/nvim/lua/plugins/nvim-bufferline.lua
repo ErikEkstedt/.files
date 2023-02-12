518d186e2dbc090ab0144dd653b6486a8742f1bd
Erik <eeckee@gmail.com>
2023-02-12 14:08:24 +0100

    nvim: bufferline ok for now

--- nvim/.config/nvim/lua/plugins/nvim-bufferline.lua
+++ nvim/.config/nvim/lua/plugins/nvim-bufferline.lua
@@ -1,95 +1,108 @@
-local fill = "#050505"
-local bg = "#151515"
--- local sbg = "#303030"
-local sbg = "none"
-
+local highlights = function()
+  local fg = "#ffffff"
+  local bg = "#303030"
+  local text = "#717171"
+  local sep = "#515151"
+  return {
+    background = {
+      bg = bg,
+      fg = text
+    },
+    buffer_selected = {
+      bg = bg,
+      fg = fg,
+      bold = true
+    },
+    separator = {
+      bg = bg,
+      fg = sep
+    },
+    separator_visible = {
+      bg = bg,
+      fg = sep
+    },
+    separator_selected = {
+      bg = bg,
+      fg = sep,
+      bold = true
+    },
+    buffer_visible = {
+      bg = bg
+    },
+    close_button = {
+      bg = bg
+    },
+    close_button_selected = {
+      bg = bg
+    },
+    duplicate = {
+      bg = bg
+    },
+    duplicate_selected = {
+      bg = bg,
+      fg = fg,
+      bold = true
+    },
+    duplicate_visible = {
+      bg = bg
+    },
+    diagnostic = {
+      bg = bg
+    },
+    diagnostic_selected = {
+      bg = bg
+    },
+    indicator_visible = {
+      bg = bg
+    },
+    indicator_selected = {
+      bg = bg,
+      fg = fg
+    },
+    modified = {
+      bg = bg
+    },
+    modified_selected = {
+      bg = bg
+    },
+    offset_separator = {
+      bg = bg,
+      fg = fg
+    },
+    pick_selected = {
+      bg = bg,
+      fg = "#c3e88d"
+    },
+    pick = {
+      bg = bg,
+      fg = "#c3e88d"
+    }
+  }
+end
 return {
   "akinsho/nvim-bufferline.lua",
   dependencies = "kyazdani42/nvim-web-devicons",
   config = function()
-    require("bufferline").setup {
-      options = {
-        view = "multiwindow",
-        numbers = "none",
-        buffer_close_icon = "",
-        modified_icon = "●",
-        close_icon = "",
-        left_trunc_marker = "",
-        right_trunc_marker = "",
-        max_name_length = 25,
-        max_prefix_length = 15,
-        tab_size = 22,
-        -- diagnostics = "nvim_lsp",
-        show_buffer_close_icons = true,
-        show_close_icon = true,
-        show_tab_indicators = false,
-        separator_style = "thin",
-        enforce_regular_tabs = true,
-        always_show_bufferline = true,
-        offsets = {
-          {
-            filetype = "NvimTree",
-            text = function()
-              return vim.fn.getcwd()
-            end,
-            highlight = "Directory",
-            text_align = "left"
+    require("bufferline").setup(
+      {
+        options = {
+          always_show_bufferline = true,
+          buffer_close_icon = "",
+          modified_icon = "●",
+          close_icon = "",
+          left_trunc_marker = "",
+          right_trunc_marker = "",
+          separator_style = {"┃", "┃"},
+          show_tab_indicators = false,
+          indicator = {
+            -- icon = "┃",
+            -- icon = "█ ",
+            style = "none"
           }
         },
-        highlights = {
-          -- Selected
-          tab_selected = {gb = sbg},
-          buffer_selected = {gb = sbg},
-          close_button_selected = {gb = sbg},
-          separator_selected = {gb = sbg},
-          indicator_selected = {fg = "#00b5ff", gb = sbg},
-          modified_selected = {gb = sbg},
-          -- Others
-          fill = {gb = fill},
-          background = {gb = bg},
-          tab = {gb = bg},
-          tab_close = {gb = bg},
-          buffer_visible = {gb = bg},
-          close_button = {gb = bg},
-          close_button_visible = {gb = bg},
-          duplicate = {gb = bg},
-          modified = {gb = bg},
-          modified_visible = {gb = bg},
-          separator = {gb = fill},
-          pick = {fg = "#fff755", gui = "bold"},
-          -- lsp selected
-          diagnostic_selected = {gb = sbg},
-          hint_selected = {gb = sbg},
-          hint_diagnostic_selected = {gb = sbg},
-          info_selected = {gb = sbg},
-          info_diagnostic_selected = {gb = sbg},
-          warning_selected = {gb = sbg},
-          warning_diagnostic_selected = {gb = sbg},
-          error_selected = {gb = sbg},
-          error_diagnostic_selected = {gb = sbg},
-          -- lsp other
-          diagnostic = {gb = bg},
-          diagnostic_visible = {gb = bg},
-          hint = {gb = bg},
-          hint_visible = {gb = bg},
-          hint_diagnostic = {gb = bg},
-          hint_diagnostic_visible = {gb = bg},
-          info = {gb = bg},
-          info_visible = {gb = bg},
-          info_diagnostic = {gb = bg},
-          info_diagnostic_visible = {gb = bg},
-          warning = {gb = bg},
-          warning_visible = {gb = bg},
-          warning_diagnostic = {gb = bg},
-          warning_diagnostic_visible = {gb = bg},
-          error = {gb = bg},
-          error_visible = {gb = bg},
-          error_diagnostic = {gb = bg},
-          error_diagnostic_visible = {gb = bg}
-        }
+        highlights = highlights
       }
-      -- TODO: Not working: see :help bufferline-offset
-    }
+    )
     vim.api.nvim_set_keymap("n", "<Space>m", ":BufferLinePick<CR>", {noremap = true, silent = true})
   end
 }
