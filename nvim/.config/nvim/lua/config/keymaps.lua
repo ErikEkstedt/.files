9611bde0ca196b87c03bbfb7b2800bc212e17f1d
Erik Ekstedt <erik@Eriks-MacBook-Pro.local>
2023-12-28 19:48:04 +0100

    added more of my 'muscle memory' keymaps and options (python)

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -6,20 +6,49 @@ local tmux = require("custom.tmux")
 local ns = { noremap = true, silent = true }
 
 -- Tmux movement
+vim.keymap.set("n", "<C-w>z", tmux.zoom, ns)
 vim.keymap.set("n", "<M-h>", tmux.move_left, ns)
 vim.keymap.set("n", "<M-j>", tmux.move_down, ns)
 vim.keymap.set("n", "<M-k>", tmux.move_up, ns)
 vim.keymap.set("n", "<M-l>", tmux.move_right, ns)
 
-vim.keymap.set("n", "<C-w>z", tmux.zoom, ns)
-
 -- General
+vim.keymap.set("n", "<C-q>", "<cmd>q<CR>", ns)
+vim.keymap.set("n", "<M-q>", "<cmd>qa!<CR>", ns)
+vim.keymap.set("n", "ga", "za", ns)
+
+-- Diagnostics
+vim.keymap.set("n", "<leader>dd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
+vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, { desc = "Next Diagnostics" })
+vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, { desc = "Prev Diagnostics" })
+
 -- Move end/start of line
 vim.keymap.del("n", "<S-h>")
 vim.keymap.del("n", "<S-l>")
 vim.keymap.set("n", "L", "$", ns)
 vim.keymap.set("n", "H", "^", ns)
 
--- vim.keymap.set("n", "<leader><leader>", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+-- Goto next under cursor
+vim.keymap.set("n", "gn", "*zvzz", ns)
+vim.keymap.set("n", "gN", "#zvzz", ns)
+
+-- */# stays on current word
+vim.keymap.set("n", "*", "*<C-o>", ns)
+vim.keymap.set("n", "#", "#<C-o>", ns)
+
+-- TODO: macro?
+-- split lines
+vim.keymap.set("n", "gl", "f, a<CR><esc>", ns)
+
+-- Buffers
 vim.keymap.set("n", "<Leader><Leader>", ":b#<CR>", ns)
-vim.keymap.set("n", "<C-q>", "<cmd>qa<CR>", ns)
+vim.keymap.set("n", "<Leader>n", ":bnext<CR>", ns)
+vim.keymap.set("n", "<Leader>N", ":bl<CR>", ns)
+vim.keymap.set("n", "<Leader>p", ":bprev<CR>", ns)
+vim.keymap.set("n", "<Leader>P", ":bf<CR>", ns)
+vim.keymap.del("n", "<Leader>bb")
+vim.keymap.set("n", "<Leader>bb", ":bprev<CR>", ns)
+vim.keymap.set("n", "<Leader>D", ":bd<CR>", ns)
+
+-- Fold
+vim.keymap.set("n", "<Leader>z", "za", ns)
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- Undercurl
+vim.g.loaded_python_provider = 0
+vim.g.python3_host_prog = "$HOME/miniconda3/bin/python"
+vim.g.python_host_bin = vim.fn.expand("$HOME/miniconda3/envs/neovim3/bin")
+
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
 vim.cmd([[let &t_Ce = "\e[4:0m"]])
