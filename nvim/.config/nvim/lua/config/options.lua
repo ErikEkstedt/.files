2e318b1be1e06407184097e55400a79016789966
Erik Ekstedt <erik@Eriks-MacBook-Pro.local>
2023-12-29 14:09:15 +0100

    nvim: options. Add winsize and winpos to sessionoptions

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -6,5 +6,8 @@ vim.g.loaded_python_provider = 0
 vim.g.python3_host_prog = "$HOME/miniconda3/bin/python"
 vim.g.python_host_bin = vim.fn.expand("$HOME/miniconda3/envs/neovim3/bin")
 
+-- vim.opt/vim.o
+vim.o.sessionoptions = "buffers,help,tabpages,winsize,winpos,terminal"
+
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
 vim.cmd([[let &t_Ce = "\e[4:0m"]])
