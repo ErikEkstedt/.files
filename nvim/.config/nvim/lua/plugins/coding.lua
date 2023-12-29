a628aa6a80faa192afd4b82c26ed5b3b7f92d789
Erik Ekstedt <erik@Eriks-MacBook-Pro.local>
2023-12-29 14:10:44 +0100

    nvim: coding
    fixed custom brackets for surround actions (I never want space between the text and brackets). Added vCoolor to insert colors.

--- nvim/.config/nvim/lua/plugins/coding.lua
+++ nvim/.config/nvim/lua/plugins/coding.lua
@@ -1,50 +1,20 @@
 return {
   {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    build = ":Copilot auth",
+    "echasnovski/mini.surround",
     opts = {
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        debounce = 75,
-        keymap = {
-          accept = "<C-l>",
-        },
-      },
-      panel = { enabled = false },
-      filetypes = {
-        markdown = true,
-        help = true,
+      custom_surroundings = {
+        ["("] = { input = { "%b()", "^.%s*().-()%s*.$" }, output = { left = "(", right = ")" } },
+        ["["] = { input = { "%b[]", "^.%s*().-()%s*.$" }, output = { left = "[", right = "]" } },
+        ["{"] = { input = { "%b{}", "^.%s*().-()%s*.$" }, output = { left = "{", right = "}" } },
+        ["<"] = { input = { "%b<>", "^.%s*().-()%s*.$" }, output = { left = "<", right = ">" } },
       },
     },
   },
   {
-    "nvim-cmp",
-    dependencies = {
-      {
-        "zbirenbaum/copilot-cmp",
-        dependencies = "copilot.lua",
-        config = function(_, opts)
-          local copilot_cmp = require("copilot_cmp")
-          copilot_cmp.setup(opts)
-          -- attach cmp source whenever copilot attaches
-          -- fixes lazy-loading issues with the copilot cmp source
-          require("lazyvim.util").lsp.on_attach(function(client)
-            if client.name == "copilot" then
-              copilot_cmp._on_insert_enter({})
-            end
-          end)
-        end,
-      },
-    },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      table.insert(opts.sources, 1, {
-        name = "copilot",
-        group_index = 1,
-        priority = 100,
-      })
+    "KabbAmine/vCoolor.vim",
+    config = function()
+      vim.g.vcoolor_disable_mappings = 1
+      vim.keymap.set("n", "<space>cc", ":VCoolor<CR>", { noremap = true, silent = true })
     end,
   },
 }
