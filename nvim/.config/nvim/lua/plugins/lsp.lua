7c43bc64f77e15e531def42bd8554e426b4866ff
Erik Ekstedt <erik@Eriks-MacBook-Pro.local>
2023-12-29 14:11:28 +0100

    nvim: lsp + cmp + copilot
    All the lsp things in one file. They always depend on eachother (mappings etc) so simpler this way.

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,31 +1,40 @@
 return {
   {
     "L3MON4D3/LuaSnip",
-    keys = {
-      -- {
-      --   "<C-l>",
-      --   function()
-      --     return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<C-l>"
-      --   end,
-      --   expr = true,
-      --   silent = true,
-      --   mode = "i",
-      -- },
-      -- {
-      --   "<C-l>",
-      --   function()
-      --     require("luasnip").jump(1)
-      --   end,
-      --   mode = "s",
-      -- },
-      -- {
-      --   "<C-h>",
-      --   function()
-      --     require("luasnip").jump(-1)
-      --   end,
-      --   mode = { "i", "s" },
-      -- },
-    },
+    keys = function()
+      return {}
+    end,
+    config = function()
+      require("luasnip/loaders/from_vscode").lazy_load() -- required for luasnip + friendly-snippets
+      local ls = require("luasnip")
+      local types = require("luasnip.util.types")
+      local snippet = ls.snippet
+      local i = ls.insert_node
+      local f = ls.function_node
+      local fmt = require("luasnip.extras.fmt").fmt
+      ls.add_snippets("python", {
+        ls.parser.parse_snippet("printshape", 'print("${1}: ", tuple(${1}.shape))'),
+        ls.parser.parse_snippet("printdict", 'for k, v in ${1}.items():\n\tprint(f"{k}: {v}")'),
+        ls.parser.parse_snippet("fig", "fig, ax = plt.subplots(${1:1}, ${2:1})"),
+        ls.parser.parse_snippet("ifmain", 'if __name__ == "__main__":'),
+      })
+      ls.config.setup({
+        region_check_events = "InsertEnter",
+        delete_check_events = "TextChanged,InsertLeave",
+        ext_opts = {
+          [types.choiceNode] = {
+            active = {
+              virt_text = { { "", "Statement" } },
+            },
+          },
+          [types.insertNode] = {
+            active = {
+              virt_text = { { "", "Character" } },
+            },
+          },
+        },
+      })
+    end,
   },
   {
     "hrsh7th/nvim-cmp",
@@ -43,6 +52,24 @@ return {
       local luasnip = require("luasnip")
       local cmp = require("cmp")
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<C-l>"] = cmp.mapping(function(fallback)
+          if luasnip.expand_or_jumpable() then
+            -- expand the snippet if available or jump to the next snippet location
+            luasnip.expand_or_jump()
+          elseif cmp.visible() then
+            -- otherwise function as the enter key and select
+            cmp.confirm({ select = true })
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<C-h>"] = cmp.mapping(function(fallback)
+          if luasnip.expand_or_jumpable(-1) then
+            luasnip.expand_or_jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
@@ -65,6 +92,57 @@ return {
             fallback()
           end
         end, { "i", "s" }),
+      })
+      opts.experimental = {
+        ghost_text = false,
+      }
+    end,
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    opts = {
+      suggestion = {
+        enabled = false,
+        auto_trigger = true,
+        debounce = 75,
+        keymap = {
+          accept = "<C-l>",
+        },
+      },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
+    },
+  },
+  {
+    "nvim-cmp",
+    dependencies = {
+      {
+        "zbirenbaum/copilot-cmp",
+        dependencies = "copilot.lua",
+        config = function(_, opts)
+          local copilot_cmp = require("copilot_cmp")
+          copilot_cmp.setup(opts)
+          -- attach cmp source whenever copilot attaches
+          -- fixes lazy-loading issues with the copilot cmp source
+          require("lazyvim.util").lsp.on_attach(function(client)
+            if client.name == "copilot" then
+              copilot_cmp._on_insert_enter({})
+            end
+          end)
+        end,
+      },
+    },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      table.insert(opts.sources, 1, {
+        name = "copilot",
+        group_index = 1,
+        priority = 500,
       })
     end,
   },
